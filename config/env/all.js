bd5254ab38eb46d65f4943e368637b89197fc666
arve0 <arve.seljebu@gmail.com>
2014-05-20 07:58:57 +0200

    fork-me ribbon

--- config/env/all.js
+++ config/env/all.js
@@ -15,6 +15,7 @@ module.exports = {
 			css: [
 				'public/lib/bootstrap/dist/css/bootstrap.css',
 				'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+				'public/lib/github-fork-ribbon-css/gh-fork-ribbon.css',
 			],
 			js: [
 				'public/lib/angular/angular.js',
